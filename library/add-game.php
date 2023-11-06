0e18cf46173b919674e464cb525993192707cf77
CCCCCasper <144883818+CCCCCasper@users.noreply.github.com>
2023-11-06 13:53:14 +0100

    foto

--- library/add-game.php
+++ library/add-game.php
@@ -13,14 +13,36 @@
     <div id="container">
         <div id class="info"><form method="POST">
 
-            <input type="file" class="afbeelding" name="afbeelding">
+        <label for="customFileInput" class="custom-file-input">
+            <div id class="afbeelding"><input type="file" id="customFileInput" class="afbeelding" name="afbeelding" accept="image/*" required></div>
+
+            <div id="image-frame">
+            <div id class="picture"><img id="selected-image" src="" alt=""></div>
+    </div>
+
+    <script>
+        document.getElementById('customFileInput').addEventListener('change', function () {
+            const selectedImage = document.getElementById('selected-image');
+            const fileInput = this;
+
+            if (fileInput.files && fileInput.files[0]) {
+            const reader = new FileReader();
+
+                reader.onload = function (e) {
+                    selectedImage.src = e.target.result;
+                };
+
+                reader.readAsDataURL(fileInput.files[0]);
+                }
+                });
+    </script>
+        </label>
 
             <label for='naam'>Naam: </label>
             <input type='text' name='naam' required><br>
 
             <label for='release'>Release: </label>
             <input type='date' name='release'><br>
-            
 
             <input type='submit' name='submit' value='submit'>
         </div>
